5d5d7670f06759645495fa6a0f93be192975e28f
Liliia <chuiko1112@gmail.com>
2020-07-01 09:35:14 +0300

    Updates

---
+++ precache-manifest.69e8168dfcb0c68c856d001a0f792f24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5d31d25958ef481ebcda7c39633ed7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08574fe741bd1f5b4eb4",
+    "url": "./static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "b1ff2999601a0418b3ef",
+    "url": "./static/css/main.35014ca3.chunk.css"
+  },
+  {
+    "revision": "08574fe741bd1f5b4eb4",
+    "url": "./static/js/2.108d1d05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.108d1d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ff2999601a0418b3ef",
+    "url": "./static/js/main.bdb708f8.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.69e8168dfcb0c68c856d001a0f792f24.js"
 );
 
 self.addEventListener('message', (event) => {
